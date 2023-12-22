524269a03e798a40568dee299c1a072e5260d02b
Sandeep Kuamr <sandeepshakya2015@gmail.com>
2023-12-23 03:21:07 +0530

    adding basic middleware in backendcode

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// Json data limit (which is coming from backend)
+app.use(express.json({ limit: "30kb" }));
+// to parse the url in same format
+app.use(express.urlencoded({ extended: true, limit: "30kb" }));
+// use the public folder as public
+app.use(express.static("public"));
+/// to set or access cookies from browser
+app.use(cookieParser());
 
 export { app };
