e8225465b72992cf1b6b3716f3973a9a6e19308d
Sandeep Kuamr <sandeepshakya2015@gmail.com>
2023-12-23 03:11:31 +0530

    adidng the app connection in backend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-dotenv.config();
-const app = express();
+import { app } from "./app.js";
 
-connectDB();
+dotenv.config();
+
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 3000;
+    app.on("error", (error) => {
+      console.log(`[-] App conection failed: ${error.message}`);
+    });
+    app.listen(port, () => {
+      console.log(`[+] Server running on PORT : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("[-] Mongo Connection failed : " + error.message);
+    process.exit(1);
+  });
